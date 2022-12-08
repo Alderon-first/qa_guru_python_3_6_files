404080229d77c791b9c8e6e8c2cc13593da115e0
eushko <eushko@pikemedia.ru>
2022-12-08 22:59:54 +0300

    все проверки для трех видов документов

--- read_csv.py
+++ read_csv.py
@@ -1,5 +1,9 @@
 import csv
 with open('resources/username.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    print(reader)
+    headers = next(reader)
+    print(headers)
     csvfile = csv.reader(csvfile)
     for r in csvfile:
         print(r)
--- test_zip.py
+++ test_zip.py
@@ -1,7 +1,8 @@
 from zipfile import ZipFile
-import zipfile
 import os
 from PyPDF2 import PdfReader
+from openpyxl import load_workbook
+import csv
 
 # – Запаковать в zip архив несколько разных файлов: pdf, xlsx, csv;
 # – Положить его в ресурсы;
@@ -10,13 +11,16 @@ from PyPDF2 import PdfReader
 # объявление переменных
 path_ = 'resources' # директория, в которую ляжет архив
 # name_fale = 'names.csv' - для архивирования одного файла. не используется.
-root_ = 'files/' # директория, в которой лежат файлы, которые нужно положить архив
-n = os.listdir('files/') # сохранение списка имен  фалов в директории
-len_list: int = len(os.listdir('files/')) # сохранение длины списка файлов в директории
+root_ = 'files' # директория, в которой лежат файлы, которые нужно положить архив
+n = os.listdir(root_) # сохранение списка имен  фалов в директории
+len_list: int = len(os.listdir(root_)) # сохранение длины списка файлов в директории
 pdf_len = 0
 pdf_text = "text"
-
-
+value_xlsx = "text"
+headers_csv = "text"
+xlsx_size = 0
+pdf_size = 0
+cvs = 0
 #сохранение данных о файлах в папке
 
 zip_p = ZipFile(os.path.join(path_, 'zip_1.zip'), 'w') # открытие файла на запись
@@ -27,19 +31,28 @@ for root, dirs, files in os.walk(root_): # Список всех файлов и
     for file in files:
         zip_p.write(os.path.join(root, file)) # Создание относительных путей и запись файлов в архив
         if file.endswith('.pdf'): # если файл в папке - запоминаю, сколько страниц и текст с первой страницы
-            print(file.title())
+            #print(file.title())
             reader = PdfReader(os.path.join(root, file.title()))
+            pdf_size = os.path.getsize(os.path.join(root, file.title()))
             pdf_len = len(reader.pages)
             #print(len(reader.pages))
             pdf_page = reader.pages[0]
             pdf_text = pdf_page.extract_text()
             #print(text)
-        if file.endswith('.csv'):
-            print('csv')
-        if file.endswith('.xlsx'):
-            print('xlsx')
+        if file.endswith('.csv'): #запоминаю заголовки из csv
+            with open(os.path.join(root, file.title())) as csvfile:
+                csv_size = os.path.getsize(os.path.join(root, file.title()))
+                reader = csv.reader(csvfile)
+                headers_csv = str(next(reader))
+                print(headers_csv)
+        if file.endswith('.xlsx'): #запоминаю данные в определенной ячейке.
+            xlsx_size = os.path.getsize(os.path.join(root, file.title()))
+            workbook = load_workbook(os.path.join(root, file.title()))
+            sheet = workbook.active
+            value_xlsx = sheet.cell(row=3, column=2).value
         else:
-            print('нет файлов указанных типов')
+            pass
+            #print('нет файлов указанных типов')
 
 
 
@@ -61,15 +74,27 @@ for i in range(len(n)): #итерация по i где i от 0 до длинн
 
 for file in zip_p.namelist():
     if file.endswith('.pdf'):
+        pdf_size_zip = os.path.getsize(file)
         reader_zip = PdfReader(file)
         assert pdf_len == len(reader_zip.pages)
         assert pdf_text in reader_zip.pages[0].extractText()
+        assert pdf_size_zip == pdf_size
     if file.endswith('.csv'):
-        print('csv1')
+        csv_size_zip = os.path.getsize(file)
+        reader = csv.reader(file)
+        headers_csv_zip = str(next(reader))
+        assert str(headers_csv[0]) == str(headers_csv_zip[0])
+        assert csv_size_zip == csv_size
     if file.endswith('.xlsx'):
-        print('xlsx1')
+        xlsx_size_zip = os.path.getsize(file)
+        workbook = load_workbook(file)
+        sheet = workbook.active
+        value_xlsx_zip = sheet.cell(row=3, column=2).value
+        assert value_xlsx_zip == value_xlsx
+        assert xlsx_size_zip == xlsx_size
     else:
-        print('нет файлов указанных типов')
+        pass
+        #print('нет файлов указанных типов')
 
 # закрытие файла
 zip_p.close()
