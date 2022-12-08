dd1c2d0a0379bd8f2dcb3b7a329c60c00cbbb46f
eushko <eushko@pikemedia.ru>
2022-12-08 17:48:33 +0300

    assert +commit

--- test_zip.py
+++ test_zip.py
@@ -8,18 +8,32 @@ import os
 
 # объявление переменных
 path_ = 'resources/'
-
+name_fale = 'names.csv'
+root = 'files/'
 # открытие файла на запись
 zip_p = ZipFile(os.path.join(path_, 'zip_1.zip'), 'w')
 # Запись файла в архив с указанием типа архивирования
-print(os.listdir('resources/'))
-zip_p.write('names.csv', compress_type=zipfile.ZIP_DEFLATED)
+print(os.listdir('files/'))
+print(len(os.listdir('files/')))
+n = os.listdir('files/')
+len_list: int = len(os.listdir('files/'))
+# zip_p.write((os.path.join(path_, name_fale)), compress_type=zipfile.ZIP_DEFLATED)
 # Запись нескольких файлов в архив
+for root, dirs, files in os.walk('files'): # Список всех файлов и папок в директории folder
+    for file in files:
+        zip_p.write(os.path.join(root, file)) # Создание относительных путей и запись файлов в архив
 # чтение содержимого
-print(str(zip_p.namelist()))
+print(zip_p.namelist())
+len_list_zip: int = len(zip_p.namelist())
+print(len_list_zip)
 # проверка имени файла
-#assert 'names.csv' in zip_p.namelist()
+
+assert len_list == len_list_zip # папака содержит столько же файлов, сколько архив
+i = 0
+for file in n:
+    assert n[i] in str(zip_p.namelist())
+    i= i+1
 # закрытие файла
 zip_p.close()
 # удаление файла
-os.remove(os.path.join(path_, 'zip_1.zip'))
+# os.remove(os.path.join(path_, 'zip_1.zip'))
